4c6da332e23bcc528aa75a702ade80a5dcb063b5
maxima <maxima.at.work@gmail.com>
2026-06-23 17:24:21 +0300

    Add ManufacturerController with basic CRUD

--- src/main/java/com/epam/rd/autocode/assessment/appliancestore/controller/ManufacturerController.java
+++ src/main/java/com/epam/rd/autocode/assessment/appliancestore/controller/ManufacturerController.java
@@ -1,4 +1,57 @@
 package com.epam.rd.autocode.assessment.appliancestore.controller;
 
+import com.epam.rd.autocode.assessment.appliancestore.model.dto.manufacturer.CreateManufacturerRequestDto;
+import com.epam.rd.autocode.assessment.appliancestore.model.dto.manufacturer.ManufacturerResponseDto;
+import com.epam.rd.autocode.assessment.appliancestore.service.ManufacturerService;
+import jakarta.validation.Valid;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/manufacturers")
 public class ManufacturerController {
+    private final ManufacturerService manufacturerService;
+
+    @GetMapping
+    public List<ManufacturerResponseDto> getAllManufacturers() {
+        return manufacturerService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ManufacturerResponseDto getManufacturerById(@PathVariable Long id) {
+        return manufacturerService.getById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ManufacturerResponseDto createManufacturer(
+            @RequestBody @Valid CreateManufacturerRequestDto requestDto
+    ) {
+        return manufacturerService.create(requestDto);
+    }
+
+    @PutMapping("/{id}")
+    public ManufacturerResponseDto updateManufacturer(
+            @PathVariable Long id,
+            @RequestBody @Valid CreateManufacturerRequestDto requestDto
+    ) {
+        return manufacturerService.updateById(id, requestDto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteManufacturer(@PathVariable Long id) {
+        manufacturerService.deleteById(id);
+    }
 }
--- src/main/java/com/epam/rd/autocode/assessment/appliancestore/service/ManufacturerService.java
+++ src/main/java/com/epam/rd/autocode/assessment/appliancestore/service/ManufacturerService.java
@@ -1,4 +1,18 @@
 package com.epam.rd.autocode.assessment.appliancestore.service;
 
+import com.epam.rd.autocode.assessment.appliancestore.model.dto.manufacturer.CreateManufacturerRequestDto;
+import com.epam.rd.autocode.assessment.appliancestore.model.dto.manufacturer.ManufacturerResponseDto;
+import java.util.List;
+
 public interface ManufacturerService {
+    List<ManufacturerResponseDto> getAll();
+
+    ManufacturerResponseDto getById(Long id);
+
+    ManufacturerResponseDto create(CreateManufacturerRequestDto requestDto);
+
+    ManufacturerResponseDto updateById(Long id, CreateManufacturerRequestDto requestDto);
+
+    void deleteById(Long id);
+
 }
