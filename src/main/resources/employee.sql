4586f3e9014b4f24cd74469015c9113559cb4178
maxima <maxima.at.work@gmail.com>
2026-06-24 12:58:30 +0300

    Add custom query and configure multiple database profiles

--- src/main/resources/employee.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users (id, name, email, password) VALUES (3, 'John', 'john@gmail.com', '123');
-
-INSERT INTO employees (id, department) VALUES (3, 'Sales');
--- src/test/java/com/epam/rd/autocode/assessment/appliancestore/ApplianceStoreApplicationTests.java
+++ src/test/java/com/epam/rd/autocode/assessment/appliancestore/ApplianceStoreApplicationTests.java
@@ -2,8 +2,10 @@ package com.epam.rd.autocode.assessment.appliancestore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApplianceStoreApplicationTests {
     @Test
     void contextLoads() {
