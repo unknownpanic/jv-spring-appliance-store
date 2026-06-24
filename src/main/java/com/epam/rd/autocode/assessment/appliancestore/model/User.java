681206a5d1b73d57b57bfac4c79ea1ff772264cd
maxima <maxima.at.work@gmail.com>
2026-06-24 14:44:34 +0300

    Add Role entity and implement UserDetails in User entity

--- src/main/java/com/epam/rd/autocode/assessment/appliancestore/model/User.java
+++ src/main/java/com/epam/rd/autocode/assessment/appliancestore/model/User.java
@@ -2,18 +2,27 @@ package com.epam.rd.autocode.assessment.appliancestore.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Getter
@@ -24,7 +33,7 @@ import org.hibernate.annotations.SQLRestriction;
 @SQLRestriction("is_deleted=false")
 @SQLDelete(sql = "UPDATE users SET is_deleted = true WHERE id = ?")
 @Inheritance(strategy = InheritanceType.JOINED)
-public abstract class User {
+public abstract class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -36,4 +45,26 @@ public abstract class User {
     private String password;
     @Column(nullable = false)
     private boolean isDeleted;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
 }
